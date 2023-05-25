9b9e9d464a4c8331e41f5baa3c43ad273904f57b
pocari1210 <pocari.design@gmail.com>
2023-05-25 14:17:38 +0900

    マルチログイン

--- routes/web.php
+++ routes/web.php
@@ -1,7 +1,9 @@
 <?php
 
-use App\Http\Controllers\ProfileController;
 use Illuminate\Support\Facades\Route;
+use App\Http\Controllers\ProfileController;
+use App\Http\Controllers\AdminController;
+use App\Http\Controllers\AgentController;
 
 /*
 |--------------------------------------------------------------------------
@@ -15,17 +17,34 @@ use Illuminate\Support\Facades\Route;
 */
 
 Route::get('/', function () {
-    return view('welcome');
+  return view('welcome');
 });
 
 Route::get('/dashboard', function () {
-    return view('dashboard');
+  return view('dashboard');
 })->middleware(['auth', 'verified'])->name('dashboard');
 
 Route::middleware('auth')->group(function () {
-    Route::get('/profile', [ProfileController::class, 'edit'])->name('profile.edit');
-    Route::patch('/profile', [ProfileController::class, 'update'])->name('profile.update');
-    Route::delete('/profile', [ProfileController::class, 'destroy'])->name('profile.destroy');
+  Route::get('/profile', [ProfileController::class, 'edit'])->name('profile.edit');
+  Route::patch('/profile', [ProfileController::class, 'update'])->name('profile.update');
+  Route::delete('/profile', [ProfileController::class, 'destroy'])->name('profile.destroy');
 });
 
-require __DIR__.'/auth.php';
+require __DIR__ . '/auth.php';
+
+// ★admin権限でログインした場合のルートグループ★
+Route::middleware(['auth', 'roles:admin'])->group(function () {
+
+  // adminのdashboard
+  Route::get('/admin/dashboard', [AdminController::class, 'AdminDashboard'])
+    ->name('admin.dashboard');
+}); // End Group Admin Middleware
+
+
+// ★agent権限でログインした場合のルートグループ★
+Route::middleware(['auth', 'roles:agent'])->group(function () {
+
+  // agentのdashboard
+  Route::get('/agent/dashboard', [AgentController::class, 'AgentDashboard'])
+    ->name('agent.dashboard');
+}); // End Group Agent Middleware
