69d5fe60df449b9d43270b5ef2050c3f926c363c
pocari1210 <pocari.design@gmail.com>
2023-05-28 15:20:49 +0900

    雛形作成

--- routes/web.php
+++ routes/web.php
@@ -5,6 +5,7 @@ use App\Http\Controllers\ProfileController;
 use App\Http\Controllers\AdminController;
 use App\Http\Controllers\AgentController;
 use App\Http\Controllers\UserController;
+use App\Http\Controllers\Backend\PropertyTypeController;
 
 /*
 |--------------------------------------------------------------------------
@@ -93,3 +94,13 @@ Route::middleware(['auth', 'roles:agent'])->group(function () {
 // adminのログインルート
 Route::get('/admin/login', [AdminController::class, 'AdminLogin'])
   ->name('admin.login');
+
+/// Admin Group Middleware 
+Route::middleware(['auth', 'role:admin'])->group(function () {
+
+  // Property Type All Route 
+  Route::controller(PropertyTypeController::class)->group(function () {
+
+    Route::get('/all/type', 'AllType')->name('all.type');
+  });
+}); // End Group Admin Middleware
